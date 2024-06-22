bcad9535777eb504d7b4b2b7227c19d21e639506
Srividhya <srividhyareddy76@gmail.com>
2024-06-22 17:08:12 +0530

    first commit

---
+++ src/com/dhanush/Test.java
@@ -0,0 +1,31 @@
+package com.dhanush;
+
+/**
+ * This is a Test class
+ */
+public class Test {
+    /**
+     * This is a main method
+     *
+     * @param args
+     */
+    public static void main(String args[]) {
+        // Printing a number
+        String lastName = "Karnati";
+        String firstName = "Dhanush Reddy";
+        System.out.print(lastName);
+    }
+}
+/**
+ * package name convention - all lower case letters
+ * example com.technologies.dhanush
+ * <p>
+ * class name convention - camel case(starting with capital letter)
+ * example class NumberPrinter
+ * <p>
+ * method name convention - camel case(starting with lower case letter )
+ * example printNumbers
+ * <p>
+ * variable name convention - camel case(starting with lower case letter )
+ * example firstName
+ */

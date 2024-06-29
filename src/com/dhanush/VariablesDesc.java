fd48f3719940754964d8e1e16e1d684e9cdc7cb5
Dhanush2451 <Dhanush2451@gmail.com>
2024-06-29 08:04:56 +0530

    Test

---
+++ src/com/dhanush/VariablesDesc.java
@@ -0,0 +1,11 @@
+package com.dhanush;
+
+public class VariablesDesc {
+    public static void main(String args[]) {
+        byte roll_no = 2;
+        short College_code = 2451;
+        int Branch_code = 737;
+        System.out.println("Student roll no : " + roll_no + ",Student College code : " + College_code + ",Student Branch code : " + Branch_code);
+
+    }
+}
